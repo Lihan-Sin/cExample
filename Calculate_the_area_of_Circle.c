c4bb1d444af374baa4e32d2d6274fd221ad8712e
Lihan-Sin <htmlcss0707@gmail.com>
2024-08-19 14:46:54 +0600

    C program for Calculate the are of circle

--- Calculate_the_area_of_Circle.c
+++ Calculate_the_area_of_Circle.c
@@ -4,7 +4,7 @@ int main()
 float radius, area;
 printf("Enter radius of the circle (in cm):");
 scanf("%f", &radius);
-area = (3.1416)*(radius*radius);
+area = (3.1416) * (radius*radius);
 printf("Area of the circle is: %.3f square cm", area);
 return 0;
 }
